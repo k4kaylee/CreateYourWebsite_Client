3969b05c93aa0f49f0c7553212367623b687f1f9
vandrovki <102423389+vandrovki@users.noreply.github.com>
2022-04-14 20:44:19 +0300

    Initialising project

---
+++ client/src/pages/MainPage.js
@@ -0,0 +1,60 @@
+import React from 'react'
+import * as Icons from 'react-icons'
+import {FaBars} from "react-icons/bi";
+import NavBar from '../components/NavBar.js';
+export default function MainPage() {
+    return (
+        <div className='main-page'>
+
+
+
+            <div className='main-page__wrapper'>
+
+                <ul className='row columns-ul'>
+                    <li className='columns columns_first'></li>
+                    <li className='columns columns_second'></li>
+                    <li className='columns columns_third'></li>
+                    <li className='columns columns_fourth'></li>
+                    <li className='columns columns_fifth'></li>
+                    <li className='columns columns_sixth'></li>
+                </ul>
+
+                <div className='main-page__text'>
+                    <ul className='row'>
+                        <li className='main-page__subtext ralewawy shadow-blue'>В РИТМЕ ЖИЗНИ</li>
+                        <li className='main-page__subtext orbitron shadow-lemon'>BRAINCHILD</li>
+                        <li className='main-page__subtext orbitron shadow-rose '>MOTIVATION</li>
+                    </ul>
+                    <p className='main-page__title orbitron'>CREATE YOUR WEBSITE</p>
+                    {/*TODO: Сделать url для p */}
+                    <ul className='row'>
+                        <li className='main-page__subtext ralewawy shadow-lemon'>ОБРАЗ МЫШЛЕНИЯ</li>
+                        <li className='main-page__subtext orbitron shadow-rose '>STRATEGY</li>
+                        <li className='main-page__subtext orbitron shadow-blue'>INNOVATION</li>
+                        <li className='main-page__subtext ralewawy shadow-lemon'>СМЕЛЫЙ ВИЗУАЛ</li>
+                    </ul>
+                </div>
+                <NavBar/>
+
+            </div>
+
+
+
+
+            <div class="content1">
+                    <div class="block1"></div>
+                    </div>
+                    <div class="content2">
+                    <div class="absolute">
+                        <div class="block2"></div>
+
+                    </div>
+                </div>
+
+
+            <div>
+                <p>GHBJDHDJHFB</p><p>GHBJDHDJHFB</p><p>GHBJDHDJHFB</p><p>GHBJDHDJHFB</p><p>GHBJDHDJHFB</p><p>GHBJDHDJHFB</p><p>GHBJDHDJHFB</p><p>GHBJDHDJHFB</p><p>GHBJDHDJHFB</p><p>GHBJDHDJHFB</p><p>GHBJDHDJHFB</p><p>GHBJDHDJHFB</p><p>GHBJDHDJHFB</p><p>GHBJDHDJHFB</p><p>GHBJDHDJHFB</p><p>GHBJDHDJHFB</p><p>GHBJDHDJHFB</p><p>GHBJDHDJHFB</p><p>GHBJDHDJHFB</p><p>GHBJDHDJHFB</p><p>GHBJDHDJHFB</p><p>GHBJDHDJHFB</p><p>GHBJDHDJHFB</p><p>GHBJDHDJHFB</p><p>GHBJDHDJHFB</p><p>GHBJDHDJHFB</p><p>GHBJDHDJHFB</p>   <p>GHBJDHDJHFB</p><p>GHBJDHDJHFB</p><p>GHBJDHDJHFB</p><p>GHBJDHDJHFB</p><p>GHBJDHDJHFB</p><p>GHBJDHDJHFB</p><p>GHBJDHDJHFB</p><p>GHBJDHDJHFB</p><p>GHBJDHDJHFB</p><p>GHBJDHDJHFB</p><p>GHBJDHDJHFB</p><p>GHBJDHDJHFB</p><p>GHBJDHDJHFB</p><p>GHBJDHDJHFB</p><p>GHBJDHDJHFB</p><p>GHBJDHDJHFB</p><p>GHBJDHDJHFB</p><p>GHBJDHDJHFB</p><p>GHBJDHDJHFB</p><p>GHBJDHDJHFB</p><p>GHBJDHDJHFB</p><p>GHBJDHDJHFB</p><p>GHBJDHDJHFB</p><p>GHBJDHDJHFB</p><p>GHBJDHDJHFB</p><p>GHBJDHDJHFB</p><p>GHBJDHDJHFB</p>
+            </div>
+        </div>
+    )
+}
