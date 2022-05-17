6319fa7fc34bbfc94825d1620413080c1126371a
Sergey Bas <sstyleimage@gmail.com>
2022-05-17 21:10:11 +0300

    Create a registration form with email confirmation

---
+++ client/src/http/index.tsx
@@ -0,0 +1,33 @@
+import axios from 'axios';
+import { AuthResponse } from '../models/response/AuthResponse';
+export const API_URL = `http://localhost:8080/api`
+const $api = axios.create({
+    withCredentials: true, //чтобы к каждому запросу куки цеплялись автомотически
+    baseURL:API_URL //по которому будет посылать url
+})
+
+$api.interceptors.request.use((config)=>{
+    // config.headers.Authorization = `Bearer ${localStorage.getItem('token')}` //на запрос, токен храним в localStorage по ключу token
+    config.headers = {
+        Authorization: `Bearer ${localStorage.getItem('token')}` //на запрос, токен храним в localStorage по ключу token,
+    };
+    return config;
+})
+//теперь на каждый запрос будет цепляться токен 
+$api.interceptors.response.use((config)=>{
+    return config;
+}, async (error) =>{
+    const originalRequest = error.config;
+    if(error.response.status == 401 && error.config && !error.config._isRetry){
+        originalRequest._isRetry = true // + дополн проверка, чтобы если снова возвращалась 401 он всегда не делал refresh
+        try {
+            const response = await axios.get<AuthResponse>(`${API_URL}/refresh`, {withCredentials:true})
+            localStorage.setItem('token', response.data.accessToken) //прийдут некоторые токены, поэтому сохраняем их в localstorage
+            return $api.request(originalRequest);  // хранит все данныедля запроса
+        } catch (error) {
+            console.log('НЕ АВТОРИЗОВАН');
+        }
+    }
+    throw error; //если if не сработал, прокидываем наверх
+})
+export default $api;
---
+++ client/src/store/store.tsx
@@ -0,0 +1,78 @@
+import { makeAutoObservable } from "mobx";
+import { IUser } from "../models/IUser";
+import AuthService from "../services/AuthService";
+import axios from "axios";
+import { AuthResponse } from "../models/response/AuthResponse";
+import { API_URL } from "../http";
+// Взаимодействие с глобальным хранилищем
+export class Store{
+    user = {} as IUser;
+    isAuth = false;
+    isLoading = false;
+
+    constructor(){
+        makeAutoObservable(this);
+    }
+
+    setAuth(bool:boolean){
+        this.isAuth = bool; //меняем текущее значение на значение которое получаем в параметрах
+    }
+
+    setUser(user: IUser){
+        this.user = user; 
+    }
+    
+    setLoading(bool:boolean){
+        this.isLoading = bool;
+    }
+
+    async login(email: string, password: string){
+        try{
+            const response = await AuthService.login(email, password)
+            console.log(response);
+            localStorage.setItem('token', response.data.accessToken);
+            this.setAuth(true)
+            this.setUser(response.data.user)
+        }   catch(e) {
+            console.log(e); // ?-проверка на существование(typescript) из-за этого не выводятся в консоль
+        }
+    }
+
+    async registration(email: string, password: string){
+        try{
+            const response = await AuthService.registration(email, password)
+            console.log(response);
+            localStorage.setItem('token', response.data.accessToken);
+            this.setAuth(true)//если дошло, то мы зарегестрированы
+            this.setUser(response.data.user)
+        }   catch(e) {
+            console.log(e); // ?-проверка на существование(typescript)
+        }
+    }
+
+    async logout(){
+        try{
+            const response = await AuthService.logout()
+            localStorage.removeItem('token'); //удаляем из localstorage
+            this.setAuth(false)
+            this.setUser({}as IUser)
+        }   catch(e) {
+            console.log(e); // ?-проверка на существование(typescript)
+        }
+    }
+
+    async checkAuth(){
+        this.setLoading(true)
+        try{ //тут interceptor будет лишним, так как нам будет выдаваться ошибка 401, в notion написал про это
+            const response = await axios.get<AuthResponse>(`${API_URL}/refresh`, {withCredentials:true})
+            console.log(response);
+            localStorage.setItem('token', response.data.accessToken); //вызываем это так как он зарегестрирован
+            this.setAuth(true);
+            this.setUser(response.data.user)
+        }   catch(e) {
+            console.log(e); // ?-проверка на существование(typescript)
+        } finally{ //выполняется и при ошибке и если все ок
+            this.setLoading(false)
+        }
+    }
+}
